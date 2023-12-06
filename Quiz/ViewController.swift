3eb0441885417f130127b8cf19f034e8b8468497
Paul <pasha.makey@gmail.com>
2023-12-06 18:02:55 +0300

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Paul Makey on 6.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
