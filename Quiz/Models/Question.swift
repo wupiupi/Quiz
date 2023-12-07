175f1395c4588b68f2f474557dd02d6f97d381b7
Paul <pasha.makey@gmail.com>
2023-12-07 19:28:06 +0300

    Project changes

---
+++ Quiz/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Questions.swift
+//  Quiz
+//
+//  Created by Paul Makey on 7.12.23.
+//
+
+import Foundation
---
+++ Quiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by Paul Makey on 7.12.23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
